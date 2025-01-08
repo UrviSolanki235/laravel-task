5f0a8ad05ef431580c96d641836d2f53e3324e50
Urvi <urvisolanki57@gmail.com>
2025-01-08 10:35:33 +0530

    Initial commit for laravel vue setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Welcome to Vue 3 with Vite in Laravel 11!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
